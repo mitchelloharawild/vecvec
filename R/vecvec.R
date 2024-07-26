af4403d32a445720866d3ea7130f1a28d4d85d00
mitchelloharawild <mitch.ohara-wild@monash.edu>
2024-07-26 17:32:52 +1000

    Import all of vctrs namespace

--- R/vecvec.R
+++ R/vecvec.R
@@ -2,7 +2,7 @@
 new_vecvec <- function(...) {
   vecs <- list(...)
   size <- lengths(vecs)
-  vctrs::new_rcrd(
+  new_rcrd(
     list(
       i = rep(seq_along(size), size),
       x = unlist(lapply(size, seq_len))
@@ -14,13 +14,11 @@ new_vecvec <- function(...) {
 
 
 #' @export
-#' @importFrom vctrs vec_proxy
 format.vecvec <- function(x, ...) {
   out <- lapply(attr(x, "v"), format, ...)
   unlist(.mapply(function(i, x) out[[i]][[x]], vec_proxy(x), NULL))
 }
 
-#' @importFrom vctrs vec_restore vec_group_loc
 #' @export
 vec_restore.vecvec <- function(x, to, ..., i = NULL) {
   g <- vec_group_loc(x[[1L]])
@@ -32,39 +30,34 @@ vec_restore.vecvec <- function(x, to, ..., i = NULL) {
   )
 
   # Update index values
-  x$i <- c(vctrs::vec_group_id(x[[1L]]))
-  x$x[unlist(g[[2L]])] <- unlist(lapply(g[[2L]], function(i) vctrs::vec_group_id(x[[2L]][i])))
+  x$i <- c(vec_group_id(x[[1L]]))
+  x$x[unlist(g[[2L]])] <- unlist(lapply(g[[2L]], function(i) vec_group_id(x[[2L]][i])))
 
   # Restore rcrd type
   NextMethod()
 }
 
-#' @importFrom vctrs vec_cast vec_proxy
 vec_cast_from_vecvec <- function(x, to, ...) {
   out <- lapply(attr(x, "v"), vec_cast, to = to, ...)
   unlist(.mapply(function(i, x) out[[i]][[x]], vec_proxy(x), NULL))
 }
 
-#' @importFrom vctrs vec_cast
 vec_cast_to_vecvec <- function(x, to, ...) {
   new_vecvec(x)
 }
 
-#' @importFrom vctrs vec_ptype2
 #' @export
 vec_ptype2.vecvec <- function(x, y, ...) {
   new_vecvec()
 }
 
-#' @importFrom vctrs vec_ptype
 #' @export
 vec_ptype.vecvec <- function(x, ...) {
-  out <- vctrs::new_data_frame(list(i = integer(), x = integer()))
+  out <- new_data_frame(list(i = integer(), x = integer()))
   attributes(out) <- attributes(x)
   out
 }
 
-#' @importFrom vctrs vec_ptype2
 #' @export
 vec_ptype2.vecvec.vecvec <- function(x, y, ...) {
   # Combine attributes
@@ -73,18 +66,16 @@ vec_ptype2.vecvec.vecvec <- function(x, y, ...) {
 }
 
 
-#' @importFrom vctrs vec_cast field field<-
 #' @export
 vec_cast.vecvec.vecvec <- function(x, to, ...) {
   # Match attributes with combined attributes
-  field(x, "i") <- vctrs::vec_match(attr(x, "v"), attr(to, "v"))[field(x, "i")]
+  field(x, "i") <- vec_match(attr(x, "v"), attr(to, "v"))[field(x, "i")]
   # Apply unified attributes
   attributes(x) <- attributes(to)
 
   x
 }
 
-#' @importFrom vctrs vec_math
 #' @export
 vec_math.vecvec <- function(.fn, .x, ...) {
   attr(.x, "v") <- lapply(attr(.x, "v"), .Generic, .fn = .fn, ...)
@@ -92,21 +83,18 @@ vec_math.vecvec <- function(.fn, .x, ...) {
   collapse_vecvec(.x)
 }
 
-#' @importFrom vctrs vec_arith
 #' @method vec_arith vecvec
 #' @export
 vec_arith.vecvec <- function(op, x, y, ...) {
   UseMethod("vec_arith.vecvec", y)
 }
 
-#' @importFrom vctrs vec_arith
 #' @method vec_arith.vecvec vecvec
 #' @export
 vec_arith.vecvec.vecvec <- function(op, x, y, ...) {
   stop("Cannot yet perform arithmetic on two vecvecs")
 }
 
-#' @importFrom vctrs vec_arith vec_recycle vec_size
 #' @method vec_arith.vecvec default
 #' @export
 vec_arith.vecvec.default <- function(op, x, y, ...) {
