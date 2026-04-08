f0240682c2ecbbeff8b3be7274adb727af748fbf
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2026-04-09 09:38:15 +1000

    Update is.na for S7 vecvec

--- R/missing.R
+++ R/missing.R
@@ -1,10 +1,6 @@
-#' @method is.na vecvec
-#' @export
-is.na.vecvec <- function(x) {
+method(is.na, class_vecvec) <- function(x) {
   # Missing values in vecvec itself
-  vctrs::vec_detect_missing(
-    vctrs::data_frame(x = field(x, "x"), i = field(x, "i"))
-  ) | 
+  is.na(x@i) | 
   # Missing values the vecvec's vectors
   unvecvec(vecvec_apply(x, is.na))
 }
