48e63cb4b9305d7f07b0302f37a442e4a79956da
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2026-04-09 09:33:17 +1000

    Add S7 conversion/casting methods

--- R/vecvec.R
+++ R/vecvec.R
@@ -115,8 +115,35 @@ vec_ptype2.vecvec.vecvec <- function(x, y, ...) {
   new_vecvec(class = compat_class)
 }
 
-
-#' @export
-vec_cast.vecvec.vecvec <- function(x, to, ...) {
-  x
+# Casting methods
+method(convert, list(class_any, class_vecvec)) <- function(from, to, ...) {
+  vecvec(from)
 }
+method(convert, 
+  list(
+    class_vecvec,
+    new_union(
+      class_vector,
+      class_Date,
+      class_POSIXct,
+      class_POSIXlt,
+      class_data.frame
+    )
+  )
+) <- function(from, to, ...) unvecvec(from, ptype = to$constructor())
+
+method(as.logical, class_vecvec) <- function(x) convert(x, class_logical)
+method(as.raw, class_vecvec) <- function(x) convert(x, class_raw)
+method(as.integer, class_vecvec) <- function(x) convert(x, class_integer)
+# For some reason this method isn't being registered properly with S7::method<-
+# method(as.double, class_vecvec) <- function(x) convert(x, class_double)
+#' @export
+#' @method as.double vecvec::vecvec
+`as.double.vecvec::vecvec` <- function(x) convert(x, class_double)
+method(as.complex, class_vecvec) <- function(x) convert(x, class_complex)
+method(as.character, class_vecvec) <- function(x) convert(x, class_character)
+method(as.list, class_vecvec) <- function(x) convert(x, class_list)
+method(as.Date, class_vecvec) <- function(x) convert(x, class_Date)
+method(as.POSIXct, class_vecvec) <- function(x) convert(x, class_POSIXct)
+method(as.POSIXlt, class_vecvec) <- function(x) convert(x, class_POSIXlt)
+method(as.data.frame, class_vecvec) <- function(x) convert(x, class_data.frame)
