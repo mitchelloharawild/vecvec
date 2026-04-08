532309f137be710fb028092f7f0f58532c193d4d
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2026-04-08 16:34:52 +1000

    Add support for missing values in the vecvec structure itself

--- R/missing.R
+++ R/missing.R
@@ -1,5 +1,10 @@
 #' @method is.na vecvec
 #' @export
 is.na.vecvec <- function(x) {
+  # Missing values in vecvec itself
+  vctrs::vec_detect_missing(
+    vctrs::data_frame(x = field(x, "x"), i = field(x, "i"))
+  ) | 
+  # Missing values the vecvec's vectors
   unvecvec(vecvec_apply(x, is.na))
 }
