19945244448ce21dac2ca5199de35ee3366e15d5
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2026-04-09 14:54:43 +1000

    Add sorting tests

---
+++ tests/testthat/test-sort.R
@@ -0,0 +1,19 @@
+test_that("sort() increasing order", {
+  rand <- rnorm(5, sd = 5)
+  expect_equal(
+    as.numeric(sort(vecvec(1:10, rand))),
+    sort(c(1:10, rand))
+  )
+  expect_equal(
+    as.numeric(sort(sample(vecvec(1:10, rand)))),
+    sort(c(1:10, rand))
+  )
+})
+
+test_that("sort() decreasing order", {
+  vv_num <- vecvec(c(1.5,3.5,2.5,5.5,4.5), c(1,3,2,5,4))
+  expect_equal(
+    as.numeric(sort(vv_num, decreasing = TRUE)),
+    sort(as.numeric(vv_num), decreasing = TRUE)
+  )
+})
