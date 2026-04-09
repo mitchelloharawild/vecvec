1b78959f892e8cffd28090d13f0771e2a3e16ded
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2026-04-10 09:44:27 +1000

    Coercion method S3 generic consistency fixes

--- R/coercion.R
+++ R/coercion.R
@@ -16,25 +16,25 @@ method(
   )
 ) <- function(from, to, ...) unvecvec(from, ptype = to$constructor())
 
-method(as.logical, class_vecvec) <- function(x) convert(x, class_logical)
-method(as.raw, class_vecvec) <- function(x) convert(x, class_raw)
-method(as.integer, class_vecvec) <- function(x) convert(x, class_integer)
+method(as.logical, class_vecvec) <- function(x, ...) convert(x, class_logical)
+method(as.raw, class_vecvec) <- function(x, ...) convert(x, class_raw)
+method(as.integer, class_vecvec) <- function(x, ...) convert(x, class_integer)
 # For some reason this method isn't being registered properly with S7::method<-
 # method(as.double, class_vecvec) <- function(x) convert(x, class_double)
 #' @export
 #' @method as.double vecvec::vecvec
-`as.double.vecvec::vecvec` <- function(x) convert(x, class_double)
-method(as.complex, class_vecvec) <- function(x) convert(x, class_complex)
-method(as.character, class_vecvec) <- function(x) convert(x, class_character)
-method(as.Date, class_vecvec) <- function(x) convert(x, class_Date)
-method(as.data.frame, class_vecvec) <- function(x) convert(x, class_data.frame)
+`as.double.vecvec::vecvec` <- function(x, ...) convert(x, class_double)
+method(as.complex, class_vecvec) <- function(x, ...) convert(x, class_complex)
+method(as.character, class_vecvec) <- function(x, ...) convert(x, class_character)
+method(as.Date, class_vecvec) <- function(x, ...) convert(x, class_Date)
+method(as.data.frame, class_vecvec) <- function(x, ...) convert(x, class_data.frame)
 method(as.POSIXct, class_vecvec) <- function(x, tz = "", ...) {
   unvecvec(x, ptype = class_POSIXct$constructor(tz = tz))
 }
 method(as.POSIXlt, class_vecvec) <- function(x, tz = "", ...) {
   unvecvec(x, ptype = class_POSIXlt$constructor(tz = tz))
 }
-method(as.list, class_vecvec) <- function(x) {
+method(as.list, class_vecvec) <- function(x, ...) {
   x@x <- lapply(x@x, as.list)
   unvecvec(x, ptype = list())
 }
