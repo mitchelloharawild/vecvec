ba360c2aeef6ce5073013e32315dcba6d032be63
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2026-04-09 16:26:42 +1000

    Add anyDuplicated() with tests

--- R/compare.R
+++ R/compare.R
@@ -1,5 +1,4 @@
-#' @export
-`duplicated.vecvec::vecvec` <- function(x, incomparables = FALSE, ...) {
+method(duplicated, class_vecvec) <- function(x, incomparables = FALSE, ...) {
   # Special case for empty vecvec vectors
   if (length(x@x) == 0L) return(duplicated(x@i, incomparables, ...))
 
@@ -28,4 +27,32 @@
 
   x@x[unlist(loc, recursive = FALSE)] <- unlist(dup, recursive = FALSE)
   unvecvec(x, ptype = logical())
+}
+
+method(anyDuplicated, class_vecvec)<- function(x, incomparables = FALSE, ...) {
+  # Find common vector types
+  ptypes <- lapply(x@x, `[`, 0L)
+  loc  <- lapply(
+    unique(ptypes),
+    function(k) which(vapply(ptypes, identical, logical(1), k))
+  )
+
+  # Search for any duplicated values within common vector types
+  for (i in seq_along(loc)) {
+    # Compute duplicates on a single vector
+    idx <- loc[[i]]
+    vec <- unlist(x@x[idx], recursive = FALSE)
+    dup <- anyDuplicated(vec, incomparables = incomparables, ...)
+    if(dup > 0L)  {
+      # Find the actual index of the duplicated value
+      len <- c(0L, cumsum(lengths(x@x[idx[-length(idx)]])))
+      pos <- findInterval(dup, len, left.open = TRUE)
+
+      # Position on the original vector is the duplicated index minus the offset
+      # of the current vector plus the offset of all previous vectors
+      return(dup - len[pos] + sum(lengths(x@x[seq_len(loc[[i]][[pos]] - 1L)])))
+    }
+  }
+
+  0L
 }
--- tests/testthat/test-compare.R
+++ tests/testthat/test-compare.R
@@ -41,3 +41,31 @@ test_that("duplicated() on a zero-length vecvec returns logical(0)", {
 test_that("duplicated() on a single-element vecvec returns FALSE", {
   expect_identical(duplicated(vecvec(42L)), FALSE)
 })
+
+# anyDuplicated() --------------------------------------------------------------
+
+test_that("anyDuplicated() returns 0L when no duplicates", {
+  vv <- vecvec(1:10)
+  expect_identical(anyDuplicated(vv), 0L)
+})
+
+test_that("anyDuplicated() returns index of first duplicate", {
+  vv <- c(vecvec(1:10, 3, 1L))
+  expect_identical(anyDuplicated(vv), 12L)
+})
+
+test_that("anyDuplicated() fromLast returns index of last duplicate", {
+  vv <- c(vecvec(1:5, "a", 3:7))
+  expect_identical(
+    anyDuplicated(vv, fromLast = TRUE),
+    5L
+  )
+})
+
+test_that("anyDuplicated() on a zero-length vecvec returns 0L", {
+  expect_identical(anyDuplicated(vecvec()), 0L)
+})
+
+test_that("anyDuplicated() on a single-element vecvec returns 0L", {
+  expect_identical(anyDuplicated(vecvec(1L)), 0L)
+})
