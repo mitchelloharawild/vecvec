9cf7c56450a2703789ffb2c40259fb8709493bf4
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2026-04-09 15:25:33 +1000

    Allow vecvec_flatten_adj to work directly on lists of vectors

--- R/flatten.R
+++ R/flatten.R
@@ -1,12 +1,17 @@
 vecvec_flatten_adj <- function(x) {
+  if (is_vecvec(x)) {
+    x@x <- vecvec_flatten_adj(x@x)
+    return(x)
+  }
+
   # Vector prototypes
-  ptypes <- lapply(x@x, `[`, 0L)
+  ptypes <- lapply(x, `[`, 0L)
 
   i <- 1L
-  while (i < length(x@x)) {
+  while (i < length(x)) {
     if (identical(ptypes[[i]], ptypes[[i+1L]])) {
-      x@x[[i]] <- c(x@x[[i]], x@x[[i+1L]])
-      x@x <- x@x[-(i+1L)]
+      x[[i]] <- c(x[[i]], x[[i+1L]])
+      x <- x[-(i+1L)]
       ptypes <- ptypes[-(i+1L)]
     } else {
       i <- i + 1L
