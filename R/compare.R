ea6112cb872af382a0e641692d6b445bd04e496a
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2026-04-09 15:56:00 +1000

    Implement duplicated() for S7 vecvec

---
+++ R/compare.R
@@ -0,0 +1,28 @@
+#' @export
+`duplicated.vecvec::vecvec` <- function(x, incomparables = FALSE, ...) {
+  # Find common vector types
+  ptypes <- lapply(x@x, `[`, 0L)
+
+  # Analagous to vctrs::vec_group_loc
+  key <- unique(ptypes)
+  loc  <- lapply(key, function(k) which(vapply(ptypes, identical, logical(1), k)))
+
+  # Identify duplicated values within common vector types
+  dup <- lapply(loc, function(i) {
+    # Compute duplicates on a single vector
+    vec <- unlist(x@x[i], recursive = FALSE)
+    res <- duplicated(vec, incomparables = incomparables, ...)
+
+    # Restructure result into list of vectors
+    idx <- c(0L, cumsum(lengths(x@x[i])))
+    len <- length(i)
+    out <- vector("list", len)
+    for (i in seq_len(len)) {
+      out[[i]] <- res[seq(idx[i] + 1L, idx[i + 1])]
+    }
+    out
+  })
+
+  x@x[unlist(loc, recursive = FALSE)] <- unlist(dup, recursive = FALSE)
+  unvecvec(x, ptype = logical())
+}
