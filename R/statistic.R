edcec857b72b9d351d06d57855eb70042e6ffbb9
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2026-04-10 23:17:39 +1000

    Import stats for S3 method registration

--- R/statistic.R
+++ R/statistic.R
@@ -2,25 +2,22 @@ method(mean, class_vecvec) <- function(x, ...) {
   mean(unvecvec(x), ...)
 }
 
-exS3_weighted.mean <- new_external_generic("stats", "weighted.mean", "x")
 method(
-  exS3_weighted.mean,
+  weighted.mean,
   class_vecvec
 ) <- function(x, w, ...) {
   weighted.mean(unvecvec(x), w = w, ...)
 }
 
-exS3_median <- new_external_generic("stats", "median", "x")
 method(
-  exS3_median,
+  median,
   class_vecvec
 ) <- function(x, ...) {
   median(unvecvec(x), ...)
 }
 
-exS3_quantile <- new_external_generic("stats", "quantile", "x")
 method(
-  exS3_quantile, 
+  quantile, 
   class_vecvec
 ) <- function(x, ...) {
   quantile(unvecvec(x), ...)
